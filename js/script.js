4f64edf9224838f7855075c7220a2837b1acece2
Mike <ramzeszews@gmail.com>
2022-10-19 21:11:42 +0300

    +Reviews & Animations

--- js/script.js
+++ js/script.js
@@ -86,6 +86,33 @@ $(document).ready(function(){
         validateForms('#consultation form');
         validateForms('#order form');
 
+        $('input[name=phone]').mask("+38(999)999-99-99");
+        
+        $('form').submit(function(e){
+            e.preventDefault();
+            $.ajax({
+                type:'POST',
+                url: 'mailer/smart.php',
+                data: $(this).serialize()
+            }).done(function(){
+                $(this).find('input').val('');
+                $('#consultation, #order').fadeOut();
+                $('.overlay, #thanks').fadeIn();
+                $('form').trigger('reset');
+            });
+            return false;
+        })
+
+        //Scrolling
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 1000) {
+                $('.pageup').fadeIn();
+            } else {
+                $('.pageup').fadeOut();
+            }
+        });
+
+        new WOW().init();
         
 });//jQuery ends
 
@@ -105,3 +132,4 @@ document.querySelector('.prev').addEventListener('click', function () {
 document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
 });
+
