3e184a09ae7ad2b38df0c5041b4c7a43df4673ff
mikemouse <kmmyemail@ukr.net>
2023-08-15 13:31:55 +0300

    remove unused slick slider javascript

--- js/script.js
+++ js/script.js
@@ -1,19 +1,4 @@
 $(document).ready(function () {
-  //     $('.carousel__inner').slick({
-  //         speed: 500,
-  //         // adaptiveHeight: true,
-  //         prevArrow: '<button type="button" class="slick-prev"><img src="../img/carousel/left-arrow.svg"></button>',
-  //         nextArrow: '<button type="button" class="slick-next"><img src="../img/carousel/right-arrow.svg"></button>',
-  //         responsive: [
-  //             {
-  //                 breakpoint: 768,
-  //                 settings: {
-  //                     arrows: false,
-  //                     dots: true
-  //                 }
-  //             }
-  //         ]
-  //       });
   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
     $(this)
       .addClass('catalog__tab_active')
